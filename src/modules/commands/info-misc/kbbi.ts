248d474df606dd3fd7ae0b5df89f0e1bf6ea2ad6
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-04-03 16:32:16 +0700

    google -> searchweb, kbbi.ts

---
+++ src/modules/commands/info-misc/kbbi.ts
@@ -0,0 +1,58 @@
+import { MessageEmbed, Message } from "discord.js";
+import { Command, handlerLoadOptionsInterface } from "../../../handler";
+import axios from "axios";
+import { load } from "cheerio";
+const { htmlToText } = require("html-to-text");
+
+module.exports = class extends Command {
+	constructor({ prefix }: handlerLoadOptionsInterface) {
+		super("kbbi", {
+			categories: "info-misc",
+			info: "Mencari definisi kata dari [KBBI](https://kbbi.web.id/)",
+			usage: `${prefix}command/alias <...>`,
+			guildOnly: false,
+		});
+	}
+	async run(message: Message, args: string[]) {
+		if (args.length < 0)
+			return message.channel.send({
+				embed: {
+					title: "Argumen invalid!",
+					description: "Jika tidak yakin bagaimana cara menggunakan, harap cek dengan `help` command",
+				},
+			});
+
+		// get query
+		const query = args.join(` `);
+		const url = "https://kbbi.web.id/";
+		const link = url + query;
+
+		// Data
+		const { data } = await axios.get(link);
+		const $ = load(data); // parse
+
+		// Get each respective data
+		let title = $("title").text(),
+			definitionGet = $('div[id = "d1"]').html()!,
+			footer = $('div[id = "footer"]').text(),
+			notFound = $(".notfound").text();
+
+		// Jika tidak ada definisi
+		if (notFound.length !== 0)
+			return message.channel.send({
+				embed: {
+					title: "Definisi tidak ditemukan!",
+					description: "Tidak ditemukan definisi dari `" + query + "` Harap masukkan kata yang benar!",
+				},
+			});
+
+		// embed
+		const embed = new MessageEmbed()
+			.setAuthor(title.replace(/<[^>]*>?/gm, ""), "https://media.discordapp.net/attachments/799595012005822484/821354290237014056/favicon.png", link.replace(/ /g, "%20"))
+			.setDescription(`${htmlToText(definitionGet.slice(0, 2048))}`)
+			.addField(`Detail Lebih Lanjut`, `[Klik Disini](${link.replace(/ /g, "%20")})`, false)
+			.setFooter(footer);
+
+		return message.channel.send(embed);
+	}
+};
---
+++ src/modules/commands/info-misc/searchweb.ts
@@ -0,0 +1,52 @@
+import { MessageEmbed, Message } from "discord.js";
+import { Command, handlerLoadOptionsInterface } from "../../../handler";
+const sec = require("search-scraper");
+
+module.exports = class extends Command {
+	constructor({ prefix }: handlerLoadOptionsInterface) {
+		super("searchweb", {
+			aliases: ["sw"],
+			categories: "info-misc",
+			info: "Search for stuff using duckduckgo and return the results as link",
+			usage: `${prefix}command/alias <...>`,
+			guildOnly: false,
+		});
+	}
+	async run(message: Message, args: string[]) {
+		if (args.length < 1) return message.channel.send(`Please input correctly!`);
+
+		const msg = await message.channel.send(`**Searching...**`),
+			timeMsStart = Date.now(),
+			options = {
+				agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+			};
+
+		const result = await sec.duckduckgo(args.join(" "), options);
+
+		if (result.error) return msg.edit(`Error: ${result.msg}`);
+		if (result.links.length === 0) return msg.edit(`**No result found!**`);
+		msg.edit("**Finishes search!** Found " + result.links.length + " results! Time taken " + (Date.now() - timeMsStart) + "ms");
+
+		let embed = new MessageEmbed()
+			.setAuthor(message.author.username, message.author.displayAvatarURL({ format: "jpg", size: 2048 }))
+			.setTitle(`Web Search`)
+			.setDescription(`Query: ${args.join(" ")}\n\n**Found ${result.links.length} results!**`)
+			.setColor("#e37151")
+			.setFooter(`Via Duckduckgo`);
+
+		// limit to 25 results
+		let limit = result.links.length > 25 ? 25 : result.links.length;
+		for (let i = 0; i < limit; i++) {
+			// get title after the 2 //
+			let titleSplit = result.links[i].split("/");
+			let title = titleSplit.slice(2, titleSplit.length).join("/");
+
+			let toShow = `[${title}](${result.links[i]})`;
+			if (title === "") toShow = `${result.links[i]}`;
+
+			embed.addField(i + 1, toShow, true);
+		}
+
+		return message.channel.send(embed);
+	}
+};
