e1854dfa7454c70017b0aa59e6ce639ca00a8c59
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-04-04 11:59:46 +0700

    convert to ts (2)

---
+++ src/modules/commands/genshin/gday.ts
@@ -0,0 +1,134 @@
+import { MessageEmbed, Message } from "discord.js";
+import { Command, handlerLoadOptionsInterface } from "../../../handler";
+import { paginationEmbed, find_DB_Return, capitalizeFirstLetter } from "../../../local_lib/functions";
+import { Genshin_Ver } from "../../../config.json";
+import moment from "moment-timezone";
+
+module.exports = class extends Command {
+	constructor({ prefix }: handlerLoadOptionsInterface) {
+		super("gday", {
+			categories: "genshin",
+			info: "Gives information about material that can be obtained on a certain day in Genshin Impact.",
+			usage: `\`${prefix}command <day/today>\``,
+			guildOnly: true,
+		});
+	}
+
+	infoInvalid() {
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setTitle("Please enter a valid day!")
+			.setDescription(
+				"Day provided can be day name in english, it's abbreviations, number 1-7 (Monday start on 1), and also day name in Indonesia.\n\nYou can also type `today` to see today's farm and all to see the farm details on every day"
+			)
+			.setTimestamp();
+
+		return embed;
+	}
+
+	dataCharsToEmbed(data: any, message: Message) {
+		let forsaken_Rift = Array.from(data)
+			.filter((filtered: any) => filtered.domain == "Forsaken Rift")
+			.map((get: any) => `\`${get.name[0]}\``);
+
+		let farm_Type_1 = Array.from(data)
+			.filter((filtered: any) => filtered.domain == "Forsaken Rift")
+			.map((get: any) => get.book);
+
+		let taishan_Mansion = Array.from(data)
+			.filter((filtered: any) => filtered.domain == "Taishan Mansion")
+			.map((get: any) => `\`${get.name[0]}\``);
+
+		let farm_Type_2 = Array.from(data)
+			.filter((filtered: any) => filtered.domain == "Taishan Mansion")
+			.map((get: any) => get.book);
+
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
+			.setTitle(`\`${data[0].fields.farmDay}\` Farm Guide`)
+			.setDescription(`**Data are based on game v${Genshin_Ver}**\nBelow are list of characters or weapons that can get materials on the specified days`)
+			.addField(`❯\u2000\Forsaken Rift (${farm_Type_1[0]}) [${forsaken_Rift.length}]:`, forsaken_Rift.join(`, `), false)
+			.addField(`❯\u2000\Taishan Mansion (${farm_Type_2[0]}) [${taishan_Mansion.length}]:`, taishan_Mansion.join(`, `), false)
+			.addField(
+				`❯\u2000\Search on Wiki`,
+				`• [${farm_Type_1[0]}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_1[0]})\n• [${farm_Type_2[0]}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_2[0]})`
+			)
+			.setTimestamp();
+
+		return embed;
+	}
+
+	dataWeaponsToEmbed(data: any, message: Message) {
+		let cecilia_Garden: any = Array.from(data).filter((filtered: any) => filtered.domain == "Cecilia Garden");
+
+		let farm_Type_1 = Array.from(data)
+			.filter((filtered: any) => filtered.domain == "Cecilia Garden")
+			.map((get: any) => get.farm_Get);
+
+		let hidden_Palace: any = Array.from(data).filter((filtered: any) => filtered.domain == "Hidden Palace of Lianshan Formula");
+
+		let farm_Type_2 = Array.from(data)
+			.filter((filtered: any) => filtered.domain == "Hidden Palace of Lianshan Formula")
+			.map((get: any) => get.farm_Get);
+
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
+			.setTitle(`\`${data[0].farm_Day}\` Farm Guide`)
+			.setDescription(`**Data are based on game v${Genshin_Ver}**\nBelow are list of characters or weapons that can get materials on the specified days`)
+			.addField(`❯\u2000\Cecilia Garden (${farm_Type_1[0]}) [${cecilia_Garden[0].weapons.length}]:`, cecilia_Garden[0].weapons.map((x: any) => `\`${x}\``).join(", "), false)
+			.addField(
+				`❯\u2000\Hidden Palace of Lianshan Formula (${farm_Type_2[0]}) [${hidden_Palace[0].weapons.length}]:`,
+				hidden_Palace[0].weapons.map((x: any) => `\`${x}\``).join(", "),
+				false
+			)
+			.addField(
+				`❯\u2000\Search on Wiki`,
+				`• [${farm_Type_1[0]}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_1[0].replace(/\s/g, "+")})\n• [${
+					farm_Type_2[0]
+				}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_2[0].replace(/\s/g, "+")})`
+			)
+			.setTimestamp();
+
+		return embed;
+	}
+
+	sunday(message: Message) {
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
+			.setTitle("Sunday Farm Guide")
+			.setDescription("You can farm for every material on sunday!")
+			.setTimestamp();
+
+		return embed;
+	}
+
+	async run(message: Message, args: string[]) {
+		let date = moment.tz("Asia/Jakarta"),
+			pages: MessageEmbed[] = [],
+			isToday = false,
+			today = date.isoWeekday();
+
+		if (!args[0]) return message.channel.send(this.infoInvalid());
+
+		if (["sunday", "sun", "minggu", "7"].includes(args.join(" ").toLowerCase())) return message.channel.send(this.sunday(message));
+
+		if (args[0].toLowerCase() === "today") {
+			isToday = true;
+			if (today === 7) return message.channel.send(this.sunday(message));
+		}
+
+		const dataCharToday = await find_DB_Return("g_Char", { farm_Index: isToday ? today.toString() : capitalizeFirstLetter(args.join(" ").toLowerCase()) });
+		if (dataCharToday.length === 0) return message.channel.send(this.infoInvalid());
+
+		const dataWeaponToday = await find_DB_Return("g_Weapon", { farm_Index: isToday ? today.toString() : capitalizeFirstLetter(args.join(" ").toLowerCase()) });
+		if (dataWeaponToday.length === 0) return message.channel.send(this.infoInvalid());
+
+		pages.push(this.dataCharsToEmbed(dataCharToday, message));
+		pages.push(this.dataWeaponsToEmbed(dataWeaponToday, message));
+
+		paginationEmbed(message, pages);
+	}
+};
---
+++ src/modules/commands/genshin/gweapon.ts
@@ -0,0 +1,116 @@
+import { MessageEmbed, Message } from "discord.js";
+import { Command, handlerLoadOptionsInterface } from "../../../handler";
+import { paginationEmbed, find_DB_Return } from "../../../local_lib/functions";
+import { Genshin_Ver } from "../../../config.json";
+
+module.exports = class extends Command {
+	prefix;
+	constructor({ prefix }: handlerLoadOptionsInterface) {
+		super("gweapon", {
+			categories: "genshin",
+			info: "Gives some information of inputted genshin impact weapon\n\n**Input is case sensitive**",
+			usage: `\`${prefix}command <weapon name/all>\``,
+			guildOnly: true,
+		});
+		this.prefix = prefix;
+	}
+
+	infoInvalid() {
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setTitle("Please enter a valid weapon name!")
+			.setDescription(`The input is case sensitive, you can check for the weapon list by using\n\`${this.prefix}${this.name} list\``)
+			.setTimestamp();
+
+		return embed;
+	}
+
+	dataToEmbed(data: any, message: Message, args: string[]) {
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
+			.setTitle(`Some Info On ${args.join(" ")}`)
+			.addField(`Domain to Farm`, data.domain, true)
+			.addField(`Ascension Material`, data.farm_Get, true)
+			.addField(`Day to Farm`, data.farm_Day, true)
+			.addField(
+				`❯\u2000\External Links`,
+				`• [Wiki (Direct Link)](https://genshin-impact.fandom.com/wiki/${args.join("_")})\n• [Wiki (Search)](https://genshin-impact.fandom.com/wiki/Special:Search?query=${args.join(
+					"+"
+				)})`
+			);
+
+		return embed;
+	}
+
+	async getAll(message: Message) {
+		const docs = await find_DB_Return("g_Weapon", {});
+
+		let cecilia_Garden: any = Array.from(docs).filter((filtered: any) => filtered.domain == "Cecilia Garden");
+
+		let farm_Type_1 = Array.from(docs)
+			.filter((filtered: any) => filtered.domain == "Cecilia Garden")
+			.map((get: any) => get.farm_Get);
+
+		let hidden_Palace: any = Array.from(docs).filter((filtered: any) => filtered.domain == "Hidden Palace of Lianshan Formula");
+
+		let farm_Type_2 = Array.from(docs)
+			.filter((filtered: any) => filtered.domain == "Hidden Palace of Lianshan Formula")
+			.map((get: any) => get.farm_Get);
+
+		let pages = [];
+
+		pages[0] = new MessageEmbed()
+			.setColor("RANDOM")
+			.setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
+			.setDescription(`Below are lists of weapons currently in genshin impact Version ${Genshin_Ver}\n\nDomain: \`Cecilia Garden\``)
+			.addField(`❯\u2000${farm_Type_1[0]} [${cecilia_Garden[0].weapons.length}]:`, cecilia_Garden[0].weapons.map((x: any) => `\`${x}\``).join(", "), false)
+			.addField(`❯\u2000${farm_Type_1[1]} [${cecilia_Garden[1].weapons.length}]:`, cecilia_Garden[1].weapons.map((x: any) => `\`${x}\``).join(", "), false)
+			.addField(`❯\u2000${farm_Type_1[2]} [${cecilia_Garden[2].weapons.length}]:`, cecilia_Garden[2].weapons.map((x: any) => `\`${x}\``).join(", "), false)
+			.addField(
+				`❯\u2000\Search on Wiki`,
+				`• [${farm_Type_1[0]}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_1[0].replace(/\s/g, "+")})\n• [${
+					farm_Type_1[1]
+				}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_1[1].replace(/\s/g, "+")})\n• [${
+					farm_Type_1[2]
+				}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_1[2].replace(/\s/g, "+")})`
+			)
+			.setTimestamp();
+
+		pages[1] = new MessageEmbed()
+			.setColor("RANDOM")
+			.setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
+			.setDescription(`Below are lists of weapons currently in genshin impact ${Genshin_Ver}\n\nDomain: \`Hidden Palace of Lianshan Formula\``)
+			.addField(`❯\u2000${farm_Type_2[0]} [${hidden_Palace[0].weapons.length}]:`, hidden_Palace[0].weapons.map((x: any) => `\`${x}\``).join(", "), false)
+			.addField(`❯\u2000${farm_Type_2[1]} [${hidden_Palace[1].weapons.length}]:`, hidden_Palace[1].weapons.map((x: any) => `\`${x}\``).join(", "), false)
+			.addField(`❯\u2000${farm_Type_2[2]} [${hidden_Palace[2].weapons.length}]:`, hidden_Palace[2].weapons.map((x: any) => `\`${x}\``).join(", "), false)
+			.addField(
+				`❯\u2000\Search on Wiki`,
+				`• [${farm_Type_2[0]}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_2[0].replace(/\s/g, "+")})\n• [${
+					farm_Type_2[1]
+				}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_2[1].replace(/\s/g, "+")})\n• [${
+					farm_Type_2[2]
+				}](https://genshin-impact.fandom.com/wiki/Special:Search?query=${farm_Type_2[2].replace(/\s/g, "+")})`
+			)
+			.setTimestamp();
+
+		paginationEmbed(message, pages);
+	}
+
+	async run(message: Message, args: string[]) {
+		if (!args[0]) return message.channel.send(this.infoInvalid());
+
+		switch (args.join(" ").toLowerCase()) {
+			case "all":
+			case "list":
+			case "semua":
+				await this.getAll(message);
+				break;
+
+			default:
+				const docs = await find_DB_Return("g_Weapon", { weapons: args.join(" ") });
+				if (!docs[0]) return message.channel.send(this.infoInvalid());
+				else return message.channel.send(this.dataToEmbed(docs[0], message, args));
+		}
+	}
+};
