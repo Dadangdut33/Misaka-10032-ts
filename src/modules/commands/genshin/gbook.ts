ad2975c957ec28f0f07f72088d7bde71ff2b18ea
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-04-04 10:43:00 +0700

    convert to ts

---
+++ src/modules/commands/genshin/gbook.ts
@@ -0,0 +1,105 @@
+import { MessageEmbed, Message } from "discord.js";
+import { Command, handlerLoadOptionsInterface } from "../../../handler";
+import { paginationEmbed, find_DB_Return, capitalizeFirstLetter } from "../../../local_lib/functions";
+import { Genshin_Ver } from "../../../config.json";
+
+module.exports = class extends Command {
+	prefix;
+	constructor({ prefix }: handlerLoadOptionsInterface) {
+		super("gbook", {
+			categories: "genshin",
+			info: "Gives information about talent book in genshin impact",
+			usage: `\`${prefix}command <name/all>\``,
+			guildOnly: true,
+		});
+		this.prefix = prefix;
+	}
+
+	tatangSutarma(message: Message) {
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setTitle(`Information on the legendary "Tatang Sutarma"`)
+			.setDescription(
+				`Requested by the chosen one ${message.author}, Konon katanya buku ini hanya ada satu di dunia dan hanya dipegang oleh satu-satunya sang sakti yaitu Sule sendiri\n
+      Tapi, tahukah anda kalau buku tatang sutarma benar benar ada? dan buku itu ada sejak tahun 1788. Dalam buku ini terdapat terjemahan dari kitab-kitab Sunda Kuno karya pujangga besar yang meliputi 14 kitab. Keempat belas kitab kuno itu menjadi pedoman hidup sehari-hari masyarakat Jawa yang diwariskan secara turun-temurun dari generasi ke generasi. Pada saat-saat tertentu malah dijadikan bahan renungan yang menambah kekayaan rohani. Kita pantas bersyukur dengan kreatifitas dan produktifitas nenek moyang itu. Harkat dan martabat kita semakin mantab, karena mempunyai jati diri yang berupa kearifan lokal. Berbagai permasalahan yang terjadi sekarang sebenarnya bisa diatasi dengan butir-butir budaya luhur masa silam.`
+			)
+			.addField("❯\u2000Found On:", "Indonesia - Unspecified Location", true)
+			.addField("❯\u2000Day To Farm:", "Confidential Information", true)
+			.addField("❯\u2000Chapter That Are Released To Public Can Be Found On:", "https://buku-tatang-sutarman.blogspot.com/")
+			.setImage("https://cdn.discordapp.com/attachments/651015913080094724/794514667992776724/Tatang_Sutarma.gif")
+			.setFooter("Above are images of Tatang Sutarma Sighting In Public")
+			.setTimestamp();
+
+		return embed;
+	}
+
+	infoInvalid() {
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setTitle("Please enter the correct name of the book that you want to check!")
+			.setDescription("There are currently 6 books for talent upgrade in genshin impact. You can use this command to check each of their farming information")
+			.addField("❯\u2000Book Lists:", "- Gold\n- Ballad\n- Freedom\n- Prosperity\n- Resistance")
+			.addField(`You can also check every book by inputting all`, `${this.prefix}${this.name} all`)
+			.setTimestamp();
+
+		return embed;
+	}
+
+	dataToEmbed(data: any, message: Message) {
+		let chars: any[] = [];
+		Array.from(data, (newData: any) => {
+			chars.push(newData.name[0]);
+		});
+
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setTitle(`Information on "${data[0].book}" Book`)
+			.setDescription(`Requested by ${message.author}\n**Data are based on game V${Genshin_Ver}**`)
+			.addField("❯\u2000Found On:", data[0].domain, true)
+			.addField("❯\u2000Day to Farm:", data[0].fields.farmDay, true)
+			.addField("❯\u2000Character That Needs It:", chars.join(", "))
+			.setTimestamp();
+
+		return embed;
+	}
+
+	async run(message: Message, args: string[]) {
+		if (!args[0]) return message.channel.send(this.infoInvalid());
+
+		switch (args.join(" ").toLowerCase()) {
+			case "all":
+				let pages = [];
+				let temp;
+				temp = await find_DB_Return("g_Char", { book: "Gold" });
+				pages.push(this.dataToEmbed(temp, message));
+				temp = await find_DB_Return("g_Char", { book: "Ballad" });
+				pages.push(this.dataToEmbed(temp, message));
+				temp = await find_DB_Return("g_Char", { book: "Freedom" });
+				pages.push(this.dataToEmbed(temp, message));
+				temp = await find_DB_Return("g_Char", { book: "Prosperity" });
+				pages.push(this.dataToEmbed(temp, message));
+				temp = await find_DB_Return("g_Char", { book: "Resistance" });
+				pages.push(this.dataToEmbed(temp, message));
+
+				paginationEmbed(message, pages);
+				break;
+
+			case "tatang sutarma":
+			case "hidden":
+			case "rahasia negara":
+			case "rahasia":
+			case "misteri":
+			case "harta karun":
+			case "tatang":
+				message.channel.send(this.tatangSutarma(message));
+				break;
+
+			default:
+				const dataGet = await find_DB_Return("g_Char", { book: capitalizeFirstLetter(args.join(" ").toLowerCase()) });
+				if (dataGet.length === 0) return message.channel.send(this.infoInvalid());
+
+				message.channel.send(this.dataToEmbed(dataGet, message));
+				break;
+		}
+	}
+};
---
+++ src/modules/commands/genshin/gcharacter.ts
@@ -0,0 +1,154 @@
+import { MessageEmbed, Message } from "discord.js";
+import { Command, handlerLoadOptionsInterface } from "../../../handler";
+import { paginationEmbed, find_DB_Return, capitalizeFirstLetter } from "../../../local_lib/functions";
+import { Genshin_Ver } from "../../../config.json";
+
+module.exports = class extends Command {
+	prefix;
+	constructor({ prefix }: handlerLoadOptionsInterface) {
+		super("gcharacter", {
+			categories: "genshin",
+			aliases: ["gchar"],
+			info: "Gives information about character farming info in genshin impact",
+			usage: `\`${prefix}command/alias <name> or [lvlup] or [list]\``,
+			guildOnly: true,
+		});
+		this.prefix = prefix;
+	}
+
+	notFound() {
+		let embed = new MessageEmbed().setTitle("Invalid Character Name").setDescription(`For character list, check using ${this.prefix}${this.name} list`);
+		return embed;
+	}
+
+	lvlUp() {
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setTitle(":arrow_down: Materials and mora needed for lvl up")
+			.setImage("https://cdn.discordapp.com/attachments/799595012005822484/799595215521316924/EriB3vTVkAYs0HD.jpg");
+		return embed;
+	}
+
+	embedCharacter(data: any) {
+		let embed = new MessageEmbed()
+			.setColor(data.color)
+			.setAuthor(`${data.name[0]} ${data.stars}`, data.pic_Small)
+			.setDescription(`**Data are based on game v${Genshin_Ver}**\n${data.name[0]} ${data.description}`)
+			.addField("❯\u2000Talent Leveling Material:", data.fields.talent, true)
+			.addField("❯\u2000Character Ascension Material:", data.fields.charAsc, true)
+			.addField("❯\u2000World boss to gain material:", data.fields.worldBoss)
+			.addField("❯\u2000Day To Farm Talent Material:", data.fields.farmDay)
+			.addField("❯\u2000Special Passive", data.fields.passive)
+			.setImage(data.pic_Big);
+		return embed;
+	}
+
+	embedCard(data: any) {
+		let embed = new MessageEmbed()
+			.setColor(data.color)
+			.setTitle(`**Data are based on game v${Genshin_Ver}**\n:arrow_down: Tips for ${data.name[0]} ${data.stars}`)
+			.setImage(data.pic_Card);
+
+		return embed;
+	}
+
+	moreInfo() {
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setTitle(":arrow_down: Materials and Mora Needed For Leveling")
+			.setDescription(`For full character list type list in command\`${this.prefix}${this.name} list\``)
+			.setImage("https://cdn.discordapp.com/attachments/799595012005822484/799595215521316924/EriB3vTVkAYs0HD.jpg");
+		return embed;
+	}
+
+	async listChar(message: Message) {
+		let anemo: any[] = [],
+			cryo: any[] = [],
+			electro: any[] = [],
+			geo: any[] = [],
+			hydro: any[] = [],
+			pyro: any[] = [],
+			adaptive: any[] = [],
+			total;
+
+		let embed = new MessageEmbed()
+			.setColor("RANDOM")
+			.setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
+			.setTitle(`Showing Full Genshin Impact Character List`)
+			.addField(`❯\u2000\Upcoming Character`, "- Ayaka [Cryo]\n")
+			.setTimestamp();
+
+		const docs = await find_DB_Return("g_Char", {});
+
+		for (let i = 0; i < docs.length; i++) {
+			switch (docs[i].type) {
+				case "anemo":
+					anemo.push(`- ${docs[i].name[0]}`);
+					break;
+				case "cryo":
+					cryo.push(`- ${docs[i].name[0]}`);
+					break;
+				case "electro":
+					electro.push(`- ${docs[i].name[0]}`);
+					break;
+				case "geo":
+					geo.push(`- ${docs[i].name[0]}`);
+					break;
+				case "hydro":
+					hydro.push(`- ${docs[i].name[0]}`);
+					break;
+				case "pyro":
+					pyro.push(`- ${docs[i].name[0]}`);
+					break;
+				case "adaptive":
+					adaptive.push(`- ${docs[i].name[0]}`);
+					break;
+				default:
+					break;
+			}
+		}
+
+		embed.addField(`❯\u2000\Anemo Character [${anemo.length}]`, anemo.join("\n"), true);
+		embed.addField(`❯\u2000\Cryo Character [${cryo.length}]`, cryo.join("\n"), true);
+		embed.addField(`❯\u2000\Electro Character [${electro.length}]`, electro.join("\n"), true);
+		embed.addField(`❯\u2000\Geo Character [${geo.length}]`, geo.join("\n"), true);
+		embed.addField(`❯\u2000\Hydro Character [${hydro.length}]`, hydro.join("\n"), true);
+		embed.addField(`❯\u2000\Pyro Character [${pyro.length}]`, pyro.join("\n"), true);
+		embed.addField(`❯\u2000\Adaptive Character [${adaptive.length}]`, adaptive.join("\n"), true);
+
+		total = anemo.length + cryo.length + electro.length + geo.length + hydro.length + pyro.length + adaptive.length;
+
+		embed.setDescription(
+			`**Data are based on game v${Genshin_Ver}**\nThere are currently over ${total} playable character in genshin impact, each of them (aside from traveller) has their own unique element. There are 6 types of element in genshin impact. Currently Anemo has ${anemo.length} playable character, Cryo has ${cryo.length}, Electro has ${electro.length}, Geo has ${geo.length}, Hydro has ${hydro.length}, and Pyro has ${pyro.length}.`
+		);
+
+		return embed;
+	}
+
+	async run(message: Message, args: string[]) {
+		if (!args[0]) return message.channel.send("Invalid input!");
+		let pages: any[] = [];
+
+		switch (args.join(" ").toLowerCase()) {
+			case "list":
+				message.channel.send(await this.listChar(message));
+				break;
+
+			case "lvlup" || "lvl":
+				message.channel.send(this.lvlUp());
+				break;
+
+			default:
+				const docs = await find_DB_Return("g_Char", { name: capitalizeFirstLetter(args.join(" ").toLowerCase()) });
+				if (docs.length === 0) return message.channel.send(this.notFound());
+
+				pages.push(this.embedCharacter(docs[0]));
+				pages.push(this.embedCard(docs[0]));
+				pages.push(this.moreInfo());
+
+				paginationEmbed(message, pages);
+
+				break;
+		}
+	}
+};
