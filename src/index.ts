6aca1d5b4c204acc8c24df932d9c47aa276d607e
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-05-28 16:58:34 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { Request, Response } from "express";
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 10032;
 
 app.get("/", (req: Request, res: Response) => res.send("<h1>Hello World!</h1>"));
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
